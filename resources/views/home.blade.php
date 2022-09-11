a9a8ab60bc9785b800d4ace1b0b1002a2ec246c7
safeboot <mynamesafeboot@gmail.com>
2022-09-11 10:49:03 +0200

    Updated favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
         @endif
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+        <link rel="shortcut icon" href="{{ url(asset('assets/CERIT-logo.png')) }}">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
